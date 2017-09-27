50c1bc0e127e31476b3589a463c7453f38c89e3b
Anton Vakhrushin <anton_vakhrushin@mail.ru>
2017-09-27 22:05:51 +0300

    First task implemented

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,7 +5,34 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
-
+	function getFloatFirst(string) {
+		while(string.length !== 0 && isNaN(parseFloat(string))) {
+			string = string.substr(1);
+		}
+		return string;
+	}
+	let min;
+	let max;
+	let flag = true;
+	let numb;
+	string = getFloatFirst(string);
+	numb = parseFloat(string);
+	let len;
+	while (!isNaN(numb)) {
+		if (flag) {
+			flag = false;
+			min = numb;
+			max = numb;
+		} else {
+			min = Math.min(min, numb);
+			max = Math.max(max, numb);
+		}
+		len = '' + numb;
+		string = string.substr(len.length);
+		string = getFloatFirst(string);
+		numb = parseFloat(string);
+	}
+	return {min: min, max: max};
 }
 
 /* ============================================= */
