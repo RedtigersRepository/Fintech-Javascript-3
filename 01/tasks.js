fb9b952c8f6fa652937aa12576b670b5535b482c
Anton Vakhrushin <anton_vakhrushin@mail.ru>
2017-09-27 22:40:03 +0300

    Fix lint

--- 01/tasks.js
+++ 01/tasks.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /**
  * найдите минимум и максимум в любой строке
  * @param  {string} string входная строка(числа отделены от других частей строки пробелами или знаками препинания)
@@ -5,34 +6,33 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
-	function getFloatFirst(string) {
-		while(string.length !== 0 && isNaN(parseFloat(string))) {
-			string = string.substr(1);
-		}
-		return string;
-	}
-	let min;
-	let max;
-	let flag = true;
-	let numb;
-	string = getFloatFirst(string);
-	numb = parseFloat(string);
-	let len;
-	while (!isNaN(numb)) {
-		if (flag) {
-			flag = false;
-			min = numb;
-			max = numb;
-		} else {
-			min = Math.min(min, numb);
-			max = Math.max(max, numb);
-		}
-		len = '' + numb;
-		string = string.substr(len.length);
-		string = getFloatFirst(string);
-		numb = parseFloat(string);
-	}
-	return {min: min, max: max};
+  function getFloatFirst(str) {
+    let anotherStr = `${str}`;
+
+    while (anotherStr.length !== 0 && isNaN(parseFloat(anotherStr))) {
+      anotherStr = anotherStr.substr(1);
+    }
+    return anotherStr;
+  }
+
+  let min, max, flag = true;
+
+  let ans = getFloatFirst(string);
+  let numb = parseFloat(ans);
+
+  while (!isNaN(numb)) {
+    if (flag) {
+      flag = false;
+      min = numb;
+      max = numb;
+    } else {
+      min = Math.min(min, numb);
+      max = Math.max(max, numb);
+    }
+    ans = getFloatFirst(ans.substr((`${numb}`).length));
+    numb = parseFloat(ans);
+  }
+  return { min, max };
 }
 
 /* ============================================= */
